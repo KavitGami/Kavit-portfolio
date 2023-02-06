5f60f9271ca237a119b46be22a0b6c5e94be3bc9
Kavit Gami <kavit.gami@gmail.com>
2023-02-06 12:53:36 -0700

    added

---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name = "about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+            <p className='text-xl mt-20'>Hello and welcome to my website! My name is Kavit Gami, and I am a computer science student with a strong passion for programming and software development. I have a broad range of technical skills and knowledge, including proficiency in programming languages such as Python, HTML, CSS, and JavaScript.
+
+As a computer science student, I have gained a deep understanding of algorithms and software development principles, which I have applied to a variety of projects and challenges. Whether it's developing a website, creating a habit tracking app, or using regex to find all instances of Dates being used in newspaper articles, I approach each task with creativity, critical thinking, and a strong attention to detail.</p>
+            <br />
+            <p className='text-xl'>
+            In my free time, I enjoy exploring new technologies and experimenting with creative coding projects. I believe that continuous learning is a crucial part of being a successful software developer, and I am always looking for opportunities to expand my knowledge and improve my skills.
+
+This website is an expression of my passion for computer science and a platform for me to share my projects and experiences with the tech community. Whether you're a fellow computer science student, a seasoned software developer, or simply someone interested in technology, I hope you'll find my website informative and engaging.
+
+Thank you for taking the time to visit my website, and I look forward to connecting with you. If you have any questions or comments, please don't hesitate to reach out!
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import regex from '..//assets/portfolio/regex.png'
+import pos from '..//assets/portfolio/pos.png'
+import habiteer from '..//assets/portfolio/habiteer.png'
+
+const portfolio = () => {
+    const portfolio = [
+        {
+            id:1,
+            src:habiteer,
+            href:'https://github.com/CMPUT301F21T30/Habiteer',
+            title:'Habit Tracking App'
+        },
+        {
+            id:2,
+            src:regex,
+            href:'https://github.com/UOFA-INTRO-NLP-F21/assn1_fsa_regex-parz1val-13',
+            title: 'Regex'
+        },
+        {
+            id:3,
+            src:pos,
+            href:'https://github.com/UOFA-INTRO-NLP-F21/f2021-asn4-parz1val-13',
+            title: 'Hmm and POS Tagging'
+        },
+        
+    ]
+  return (
+    <div name = 'portfolio' className = 'bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
+                <p className='py-6'>
+                    Check out some of my work right here.
+                </p>
+            </div>
+
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
+            {
+                portfolio.map(({id,src,href,title})=>(
+                    <div key ={id} className='shadow-md shadow-gray-600 rounded-lg'>
+                    <img src = {src} alt = '' className='rounded-md duration-200 hover:scale-105'/>
+                   
+                    <div className='flex items-center justify-center'>
+                        <p className='py-1 text-gray-400'>{title}</p>
+                    </div>
+                    <div className='flex items-center justify-center'>
+                        <a href = {href}><button  className='w-1/2 px-6 py-2 m-2 duration-200 hover:scale-105'>Code</button></a>
+                    </div>
+                </div>  
+                ))
+            }
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default portfolio
